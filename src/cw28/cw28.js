04837a66571c5ad043a2da5d8f22cc4cbd8b6375
Czcibor44 <czcibor@gmail.com>
2019-12-22 13:46:51 +0100

    przykładowe cookies

---
+++ src/cw28/cw28.js
@@ -0,0 +1,20 @@
+var x = document.getElementById("myBtn");
+x.addEventListener("mouseover", myFunction);
+x.addEventListener("mouseout", myThirdFunction);
+x.addEventListener("click", mySecondFunction);
+
+function myFunction() {
+    document.getElementById("demo").innerHTML += "Mouse Over!<br>";
+}
+
+
+function mySecondFunction() {
+    document.getElementById("demo").innerHTML += "Mouse Clicked!<br>";
+    x.removeEventListener("click", mySecondFunction);
+}
+
+
+function myThirdFunction() {
+    document.getElementById("demo").innerHTML += "Mouse Out!<br>";
+}
+
